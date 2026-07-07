b955712e383bf1d5462c28ed602663e6971a39ae
vipinlamba1985 <vipin.lamba1985@gmail.com>
2026-07-06 22:03:48 -0400

    feat(gallery): redirect Gallery entry to Magic Library

--- app/(app)/gallery/layout.js
+++ app/(app)/gallery/layout.js
@@ -6,5 +6,8 @@ import { usePathname, useRouter } from 'next/navigation';
 export default function GalleryLayout({ children }) {
   const pathname = usePathname();
   const router = useRouter();
+  useEffect(() => {
+    if (pathname === '/gallery') router.replace('/magic-library');
+  }, [pathname, router]);
   return children;
 }
