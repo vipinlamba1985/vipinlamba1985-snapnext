5455c7f7d3a92f69da535fa0074f5d7f7251b61e
vipinlamba1985 <vipin.lamba1985@gmail.com>
2026-07-06 22:02:58 -0400

    feat(gallery): prepare gallery route layout

--- app/(app)/gallery/layout.js
+++ app/(app)/gallery/layout.js
@@ -1,3 +1,8 @@
+'use client';
+
+import { useEffect } from 'react';
+import { usePathname, useRouter } from 'next/navigation';
+
 export default function GalleryLayout({ children }) {
   return children;
 }
