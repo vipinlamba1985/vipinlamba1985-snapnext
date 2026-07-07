c4b15ce0b08a74eee17274f8bd6b25555e0ae86f
vipinlamba1985 <vipin.lamba1985@gmail.com>
2026-07-06 22:03:31 -0400

    feat(gallery): read current Gallery route

--- app/(app)/gallery/layout.js
+++ app/(app)/gallery/layout.js
@@ -4,5 +4,7 @@ import { useEffect } from 'react';
 import { usePathname, useRouter } from 'next/navigation';
 
 export default function GalleryLayout({ children }) {
+  const pathname = usePathname();
+  const router = useRouter();
   return children;
 }
