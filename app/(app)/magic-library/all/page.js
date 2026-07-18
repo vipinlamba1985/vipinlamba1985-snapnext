16ac7d825f68e3c0aae08e350bd89db25d592e7e
vipinlamba1985 <vipin.lamba1985@gmail.com>
2026-07-17 23:09:39 -0400

    Add all photos route

---
+++ app/(app)/magic-library/all/page.js
@@ -0,0 +1,7 @@
+import AllPhotosTimeline from '@/components/magic-library/AllPhotosTimeline';
+
+export const metadata = { title: 'All Photos · SnapNext' };
+
+export default function AllPhotosPage() {
+  return <AllPhotosTimeline />;
+}
